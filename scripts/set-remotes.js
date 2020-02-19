2a1f2ed303939c3a2c1cbf088b7e90582f6f028f
Cole Voss <voss.cole@gmail.com>
2020-02-19 14:56:04 -0600

    remote scripts

--- scripts/set-remotes.js
+++
@@ -1,8 +0,0 @@
-const { spawn } = require('child_process');
-
-// Child will use parent's stdios.
-spawn(
-  'git',
-  ['remote', 'add', 'base', 'git@github.com:colevoss/asdfasdf.git'],
-  { stdio: 'inherit' },
-);
